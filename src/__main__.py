aa3e95994f09902e7cacbfc9eb24aae1c5072625
Pawel Szostek <pawel.szostek@gmail.com>
2012-01-25 20:54:13 +0100

    Add --version option, (logically) nicer printing infrastructure

--- src/__main__.py
+++ src/__main__.py
@@ -149,6 +149,8 @@ def main():
             print(unknown_error) 
 
     if not sth_chosen:
+        p.rawprint("No option selected. Running automatic flow")
+        p.rawprint("To see some help, type hdlmake --help")
         kernel.run()
 
 if __name__ == "__main__":
