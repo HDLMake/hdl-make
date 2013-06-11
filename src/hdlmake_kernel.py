lerwys/hdl-make2

generate_modelsim_makefile call: fix name calling throughout the program

src/hdlmake_kernel.py
@@ -18,11 +18,12 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
 #
-
+# Modified to allow iSim simulation by Lucas Russo (lucas.russo@lnls.br)
 
 import os
 import msg as p
 import path
+import global_mod
 from help_printer import HelpPrinter as hp
 from makefile_writer import MakefileWriter
 from flow import ISEProject, ISEProjectProperty
@@ -50,17 +51,35 @@ class HdlmakeKernel(object):
             self.fetch(unfetched_only = True)
 
         if tm.action == "simulation":
-            self.generate_modelsim_makefile()
+			# Defaults to isim simulator tool
+            if global_mod.sim_tool == "isim":
+                self.generate_isim_makefile()
+            elif global_mod.sim_tool == "vsim":
+                self.generate_vsim_makefile()
+            else:
+                raise RuntimeError("Unrecognized or not specified simulation tool: "+ str(global_mod.sim_tool))
+                quit()
+			# Force declaration of sim_tool varible in Manifest
+#			if tm.sim_tool == None:
+#				p.error("sim_tool variable must be defined in the manifest")
+#				quit()
+#			# Make distintion between isim and vsim simulators
+#			if tm.sim_tool == "vsim":
+#            	self.generate_modelsim_makefile()
+#			elif tm.sim_tool == "isim":
+#				self.generate_isim_makefile()
+#			else:
+#				raise RuntimeError("Unrecognized sim tool: "+tm.sim_tool)
         elif tm.action == "synthesis":
             if tm.syn_project == None:
-                p.error("syn_project variable must be defined in the manfiest")
+                p.error("syn_project variable must be defined in the manifest")
                 quit()
             if tm.target.lower() == "xilinx":
-                self.generate_ise_project()
-                self.generate_ise_makefile()
-                self.generate_remote_synthesis_makefile()
+				self.generate_ise_project()
+				self.generate_ise_makefile()
+				self.generate_remote_synthesis_makefile()
             elif tm.target.lower() == "altera":
-                self.generate_quartus_project()
+                 self.generate_quartus_project()
 #                self.generate_quartus_makefile()
 #                self.generate_quartus_remote_synthesis_makefile()
             else:
@@ -99,20 +118,38 @@ class HdlmakeKernel(object):
         self.modules_pool.fetch_all(unfetched_only)
         p.vprint(str(self.modules_pool))
 
-    def generate_modelsim_makefile(self):
-        p.info("Generating makefile for simulation.")
+    def generate_vsim_makefile(self):
+#        p.info("Generating makefile for simulation.")
+        p.info("Generating ModelSim makefile for simulation.")
+        solver = DependencySolver()
+
+        pool = self.modules_pool
+        if not pool.is_everything_fetched():
+          p.echo("A module remains unfetched. "
+            "Fetching must be done prior to makefile generation")
+          p.echo(str([str(m) for m in self.modules_pool.modules if not m.isfetched]))
+          quit()
+        top_module = pool.get_top_module()
+        flist = pool.build_global_file_list();
+        flist_sorted = solver.solve(flist);
+        #self.make_writer.generate_modelsim_makefile(flist_sorted, top_module)
+        self.make_writer.generate_vsim_makefile(flist_sorted, top_module)
+
+    def generate_isim_makefile(self):
+#        p.info("Generating makefile for simulation.")
+        p.info("Generating ISE Simulation (ISim) makefile for simulation.")
         solver = DependencySolver()
 
         pool = self.modules_pool
         if not pool.is_everything_fetched():
             p.echo("A module remains unfetched. "
-                "Fetching must be done prior to makefile generation")
+                "Fetching must be done prior to makefile generation. Try issuing \"hdlmake2 --fetch\"")
             p.echo(str([str(m) for m in self.modules_pool.modules if not m.isfetched]))
             quit()
         top_module = pool.get_top_module()
         flist = pool.build_global_file_list();
         flist_sorted = solver.solve(flist);
-        self.make_writer.generate_modelsim_makefile(flist_sorted, top_module)
+        self.make_writer.generate_isim_makefile(flist_sorted, top_module)
 
     def generate_ise_makefile(self):
         p.info("Generating makefile for local synthesis.")

src/srcfile.py
@@ -103,7 +103,7 @@ class SourceFile(IDependable, File):
 class VHDLFile(SourceFile):
         def __init__(self, path, library = None, vcom_opt = None):
                 SourceFile.__init__(self, path, library);
-                ##self.__create_deps();
+                self.__create_deps();
                 if not vcom_opt:
                     self.vcom_opt = ""
                 else:
@@ -134,12 +134,25 @@ class VHDLFile(SourceFile):
                 non-standard library a tuple (lib, file) is returned in a list.
 
                 """
+                # Modification here! global_mod.top_module.action does not
+                # get set for top module in time. FIX this
+                std_libs = ['std', 'ieee']
                 if global_mod.top_module.action == "simulation":
                     try:
-                        std_libs = flow.ModelsiminiReader().get_libraries()
-                    except RuntimeError:
-                        std_libs =  flow.MODELSIM_STANDARD_LIBS
+                        if global_mod.sim_tool == "isim":
+                            std_libs = flow.XilinxsiminiReader().get_libraries()
+                        elif global_mod.sim_tool == "vsim":
+                            std_libs = flow.ModelsiminiReader().get_libraries()
+                        else:   # global_mod.sim_tool == None:
+                            p.warning("Could not determine simulation tool. Defaulting to ISim")
+                            std_libs =  flow.ISIM_STARDAND_LIBS
+                    except RuntimeError as e:
+                        #std_libs =  flow.MODELSIM_STANDARD_LIBS
+                        print "I/O error: ({0})".format(e.message)
+                        p.error("Picking standard ISim simulation libraries. Try to fix the error.")
+                        std_libs =  flow.ISIM_STARDAND_LIBS
                 elif global_mod.top_module.action == "synthesis":
+                    print("setting std libs for synthesis...")
                     if global_mod.top_module.target == "xilinx":
                         std_libs = flow.ISE_STANDARD_LIBS
                     elif global_mod.top_module.target == "altera":
@@ -163,18 +176,27 @@ class VHDLFile(SourceFile):
                         if m != None:
                                 use_lines.append(m.group(1))
 
+
+                #print("use_lines: " + ' '.join(use_lines))
+                #print("std_libs: " + ' '.join(std_libs))
                 ret = set() 
                 for line in use_lines:
                         m = re.match(lib_pattern, line)
                         if m != None:
-                                #omit standard libraries
+                                #omit standard libraries. Fix this. Ignore ISim std libraries
+                                #print("m.group(1): " + (m.group(1)).lower())
+                                #print("std_libs: " + ' '.join(std_libs))
                                 if (m.group(1)).lower() in std_libs:
-                                        continue
+                                    
+                                    continue
+                                #print("Did not take the continue statement")
                                 if self.library != "work":
                                     #if a file is put in a library, `work' points this library
                                     new = (self.library.lower(), m.group(2).lower())
                                 else:
                                     new = (m.group(1).lower(), m.group(2).lower())
+
+                                #p.vprint("new: " + ' '.join(new))
                                 #dont add if the tuple is already in the list
                                 if new in self.dep_provides:
                                     continue
@@ -350,7 +372,8 @@ class SourceFileFactory:
                         nf = VHDLFile(path, library, vcom_opt)
                 elif extension == 'v' or extension == 'vh' or extension == 'vo':
                         nf = VerilogFile(path, library, vlog_opt, include_dirs)
-                elif extension == 'sv':
+                # Fixed missing svh extension
+                elif extension == 'sv' or extension == 'svh':
                         nf = SVFile(path, library, vlog_opt, include_dirs)
                 elif extension == 'ngc':
                         nf = NGCFile(path)

HDLMake/hdl-make

src/hdlmake_kernel.py
@@ -18,7 +18,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
 #
-
+# Modified to allow iSim simulation by Lucas Russo (lucas.russo@lnls.br)
 
 import os
 import msg as p
@@ -51,18 +51,23 @@ class HdlmakeKernel(object):
         if tm.action == "simulation":
             if tm.use_compiler == "iverilog":
                 self.generate_iverilog_makefile()
+            elif tm.use_compiler == "isim" :
+                self.generate_isim_makefile()
+            elif tm.use_compiler == "vsim" or tm.use_compiler == "modelsim":
+                self.generate_vsim_makefile()
             else:
-                self.generate_modelsim_makefile()
+                raise RuntimeError("Unrecognized or not specified simulation tool: "+ str(tm.use_compiler))
+                quit()
         elif tm.action == "synthesis":
             if tm.syn_project == None:
-                p.error("syn_project variable must be defined in the manfiest")
+                p.error("syn_project variable must be defined in the manifest")
                 quit()
             if tm.target.lower() == "xilinx":
                 self.generate_ise_project()
                 self.generate_ise_makefile()
                 self.generate_remote_synthesis_makefile()
             elif tm.target.lower() == "altera":
-                self.generate_quartus_project()
+                 self.generate_quartus_project()
 #                self.generate_quartus_makefile()
 #                self.generate_quartus_remote_synthesis_makefile()
             else:
@@ -99,20 +104,38 @@ class HdlmakeKernel(object):
         self.modules_pool.fetch_all(unfetched_only)
         p.vprint(str(self.modules_pool))
 
-    def generate_modelsim_makefile(self):
-        p.info("Generating makefile for simulation.")
+    def generate_vsim_makefile(self):
+#        p.info("Generating makefile for simulation.")
+        p.info("Generating ModelSim makefile for simulation.")
+        solver = DependencySolver()
+
+        pool = self.modules_pool
+        if not pool.is_everything_fetched():
+          p.echo("A module remains unfetched. "
+            "Fetching must be done prior to makefile generation")
+          p.echo(str([str(m) for m in self.modules_pool.modules if not m.isfetched]))
+          quit()
+        top_module = pool.get_top_module()
+        flist = pool.build_global_file_list();
+        flist_sorted = solver.solve(flist);
+        #self.make_writer.generate_modelsim_makefile(flist_sorted, top_module)
+        self.make_writer.generate_vsim_makefile(flist_sorted, top_module)
+
+    def generate_isim_makefile(self):
+#        p.info("Generating makefile for simulation.")
+        p.info("Generating ISE Simulation (ISim) makefile for simulation.")
         solver = DependencySolver()
 
         pool = self.modules_pool
         if not pool.is_everything_fetched():
             p.echo("A module remains unfetched. "
-                "Fetching must be done prior to makefile generation")
+                "Fetching must be done prior to makefile generation. Try issuing \"hdlmake2 --fetch\"")
             p.echo(str([str(m) for m in self.modules_pool.modules if not m.isfetched]))
             quit()
         top_module = pool.get_top_module()
-        flist = pool.build_global_file_list()
-        flist_sorted = solver.solve(flist)
-        self.make_writer.generate_modelsim_makefile(flist_sorted, top_module)
+        flist = pool.build_global_file_list();
+        flist_sorted = solver.solve(flist);
+        self.make_writer.generate_isim_makefile(flist_sorted, top_module)
 
     def generate_iverilog_makefile(self):
         from dep_solver import DependencySolver

src/srcfile.py
@@ -18,12 +18,13 @@
 #    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
 #
 
-from dep_solver import IDependable 
+from dep_solver import IDependable
 import os
 import msg as p
 import global_mod
 import flow
 import path as path_mod
+
 
 class File(object):
     def __init__(self, path):
@@ -133,12 +134,26 @@ class VHDLFile(SourceFile):
         non-standard library a tuple (lib, file) is returned in a list.
 
         """
+        # Modification here! global_mod.top_module.action does not
+        # get set for top module in time. FIX this
+
+        std_libs = ['std', 'ieee']
         if global_mod.top_module.action == "simulation":
             try:
-                std_libs = flow.ModelsiminiReader().get_libraries()
-            except RuntimeError:
-                std_libs =  flow.MODELSIM_STANDARD_LIBS
+                if global_mod.top_module.use_compiler == "isim":
+                    std_libs = flow.XilinxsiminiReader().get_libraries()
+                elif global_mod.top_module.use_compiler == "vsim":
+                    std_libs = flow.ModelsiminiReader().get_libraries()
+                else:
+                    p.warning("Could not determine simulation tool. Defaulting to Modelsim")
+                    std_libs = flow.MODELSIM_STANDARD_LIBS
+            except RuntimeError as e:
+             #std_libs =  flow.MODELSIM_STANDARD_LIBS
+                print "I/O error: ({0})".format(e.message)
+                p.error("Picking standard Modelsim simulation libraries. Try to fix the error.")
+                std_libs = flow.MODELSIM_STARDAND_LIBS
         elif global_mod.top_module.action == "synthesis":
+            print("setting std libs for synthesis...")
             if global_mod.top_module.target == "xilinx":
                 std_libs = flow.ISE_STANDARD_LIBS
             elif global_mod.top_module.target == "altera":
@@ -151,7 +166,7 @@ class VHDLFile(SourceFile):
         except UnicodeDecodeError:
             return []
 
-        use_pattern = re.compile("^[ \t]*use[ \t]+([^ ]+)[ \t]*.*$")
+        use_pattern = re.compile("^[ \t]*use[ \t]+([^; ]+)[ \t]*;.*$")
         lib_pattern = re.compile("([^.]+)\.([^.]+)\.all")
 
         use_lines = []
@@ -159,7 +174,7 @@ class VHDLFile(SourceFile):
             #identifiers and keywords are case-insensitive in VHDL
             line_lower = line.lower()
             m = re.match(use_pattern, line_lower)
-            if m != None:
+            if m is not None:
                 use_lines.append(m.group(1))
 
         ret = set() 
@@ -286,48 +301,40 @@ class VerilogFile(SourceFile):
 
         
 class SVFile(VerilogFile):
-    def __init__(self, path, library = None, vlog_opt = None, include_dirs = None):
+    def __init__(self, path, library=None, vlog_opt=None, include_dirs= None):
         VerilogFile.__init__(self, path, library, vlog_opt, include_dirs)
 
 
 class UCFFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class TCLFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class XISEFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class CDCFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class SignalTapFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class SDCFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class QIPFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class DPFFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class NGCFile(SourceFile):
@@ -337,8 +344,7 @@ class NGCFile(SourceFile):
 
 
 class WBGenFile(File):
-    def __init__(self, path):
-        File.__init__(self, path)
+    pass
 
 
 class SourceFileSet(list):
@@ -388,8 +394,8 @@ class SourceFileSet(list):
 
 
 class SourceFileFactory:
-    def new (self, path, library=None, vcom_opt=None, vlog_opt=None, include_dirs=None):
-        if path == None or path == "":
+    def new(self, path, library=None, vcom_opt=None, vlog_opt=None, include_dirs=None):
+        if path is None or path == "":
             raise RuntimeError("Expected a file path, got: "+str(path))
         if not os.path.isabs(path):
             path = os.path.abspath(path)