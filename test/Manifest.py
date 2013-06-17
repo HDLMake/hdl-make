2839797d21ee3c8c3c29e8c687d4fb5e7d2d9084
Lucas Russo <lerwys@gmail.com>
2013-06-17 16:45:52 -0300

    test/*: fix isim testbench

--- test/Manifest.py
+++ test/Manifest.py
@@ -1,6 +1,4 @@
 action = "simulation"
 target = "xilinx"
 
-#files = [ "dbe_demo_top_sim.vhd", "dbe_demo_top_sim_tb.vhd" ];
 files = [ "top_module_tb.vhd", "top_module.vhd" ]
-modules = { "local" : ["../../../cores/general-cores" ] };
