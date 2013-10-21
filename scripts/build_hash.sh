cb90d0adedd7853b39ad622f56cef85a1e31abd5
Pawel Szostek <pawel.szostek@cern.ch>
2013-10-21 14:58:40 +0200

    Move makefile and "build" scripts to separate directory

--- scripts/build_hash.sh
+++ scripts/build_hash.sh
@@ -8,5 +8,5 @@ date_string=$(echo $date_line | awk '{print $6 $3 $4}')
 
 embed_string='"'$(echo "$date_string:${commit_string:0:6}")'"'
 
-build_hash_path="src/build_hash.py"
+build_hash_path="../hdlmake/build_hash.py"
 echo 'BUILD_ID = '"$embed_string" > $build_hash_path
