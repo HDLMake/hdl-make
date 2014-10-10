aabe6c840ed4d8414dd62bb67781e10672184103
Adrian Fiergolski <Adrian.Fiergolski@cern.ch>
2014-10-10 11:14:54 +0200

    Disable optimisations in order to have visibility of counter's signal.

--- tests/counter/sim/modelsim/verilog/Manifest.py
+++ tests/counter/sim/modelsim/verilog/Manifest.py
@@ -2,7 +2,7 @@ action = "simulation"
 sim_tool = "modelsim"
 top_module = "counter_tb"
 
-sim_post_cmd = "vsim -do ../vsim.do -i counter_tb"
+sim_post_cmd = "vsim -novopt -do ../vsim.do -i counter_tb"
 
 modules = {
   "local" : [ "../../../testbench/counter_tb/verilog" ],
