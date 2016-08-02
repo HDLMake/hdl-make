2aee04b8aa2fc0346cfc1d86d1919ca1301c6e78
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2016-08-03 00:18:00 +0200

    Add the -g2012 option to the VHDL demo for IVerilog

--- tests/counter/sim/iverilog/vhdl/Manifest.py
+++ tests/counter/sim/iverilog/vhdl/Manifest.py
@@ -2,6 +2,8 @@ action = "simulation"
 sim_tool = "iverilog"
 sim_top = "counter_tb"
 
+iverilog_opt = "-g2012"
+
 sim_pre_cmd ="echo IMPORTANT, IVerilog always needs a Verilog testbench, no matter if the DUT is written in VHDL!"
 sim_post_cmd = "vvp counter_tb.vvp; gtkwave counter_tb.vcd"
 
