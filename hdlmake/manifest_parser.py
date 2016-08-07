5e27ff6cfb7cfcfe9946be9afa5790475645afdc
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2016-08-07 07:42:10 +0200

    A more refined refactoring for manifest_parser

--- hdlmake/manifest_parser.py
+++ hdlmake/manifest_parser.py
@@ -37,229 +37,211 @@ class ManifestParser(ConfigParser):
         ConfigParser.__init__(
             self,
             description="Configuration options description")
-        self.add_option(
-            'fetchto',
-            default=None,
-            help="Destination for fetched modules",
-            type='')
-        self.add_option(
-            'fetch_pre_cmd',
-            default='',
-            help="Command to be executed before fetch",
-            type='')
-        self.add_option(
-            'fetch_post_cmd',
-            default='',
-            help="Command to be executed after fetch",
-            type='')
+        general_options = [
+            {'name': 'top_module',
+            'default': None,
+            'help': "Top level entity for synthesis and simulation",
+            'type': ''},
+            {'name': 'include_dirs',
+            'default': None,
+            'help': "Include dirs for Verilog sources",
+            'type': []},
+            {'name': 'action',
+            'default': '',
+            'help': "What is the action that should be taken if "
+                    "HDLMake is run in auto mode (simulation/synthesis)",
+            'type': ''},
+            {'name': 'library',
+            'default': "work",
+            'help': "Destination library for module's VHDL files",
+            'type': ""},
+            {'name': 'incl_makefiles',
+            'default': [],
+            'help': "List of .mk files appended to toplevel makefile",
+            'type': []},
+            {'name': 'files',
+            'default': [],
+            'help': "List of files from the current module",
+            'type': ''},
+            {'name': 'modules',
+            'default': {},
+            'help': "List of local modules",
+            'type': {}}]
+        self.add_option_list(general_options)
         self.add_delimiter()
-        self.add_option(
-            'syn_tool',
-            default=None,
-            help="Tool to be used in the synthesis",
-            type='')
-        self.add_option(
-            'syn_device',
-            default=None,
-            help="Target FPGA device",
-            type='')
-        self.add_option(
-            'syn_family',
-            default=None,
-            help="Target FPGA family",
-            type='')
-        self.add_option(
-            'syn_grade',
-            default=None,
-            help="Speed grade of target FPGA",
-            type='')
-        self.add_option(
-            'syn_package',
-            default=None,
-            help="Package variant of target FPGA",
-            type='')
-        self.add_option(
-            'syn_top',
-            default=None,
-            help="Top level module for synthesis",
-            type='')
-        self.add_option(
-            'syn_project',
-            default=None,
-            help="Project file (.xise, .ise, .qpf)",
-            type='')
-        self.add_option(
-            'syn_pre_cmd',
-            default='',
-            help="Command to be executed before synthesis: local",
-            type='')
-        self.add_option(
-            'syn_pre_synthesize_cmd',
-            default='',
-            help="Command to be executed before synthesis: synthesize",
-            type='')
-        self.add_option(
-            'syn_post_synthesize_cmd',
-            default='',
-            help="Command to be executed after synthesis: synthesize",
-            type='')
-        self.add_option(
-            'syn_pre_translate_cmd',
-            default='',
-            help="Command to be executed before synthesis: translate",
-            type='')
-        self.add_option(
-            'syn_post_translate_cmd',
-            default='',
-            help="Command to be executed after synthesis: translate",
-            type='')
-        self.add_option(
-            'syn_pre_map_cmd',
-            default='',
-            help="Command to be executed before synthesis: map",
-            type='')
-        self.add_option(
-            'syn_post_map_cmd',
-            default='',
-            help="Command to be executed after synthesis: map",
-            type='')
-        self.add_option(
-            'syn_pre_par_cmd',
-            default='',
-            help="Command to be executed before synthesis: par",
-            type='')
-        self.add_option(
-            'syn_post_par_cmd',
-            default='',
-            help="Command to be executed after synthesis: par",
-            type='')
-        self.add_option(
-            'syn_pre_bitstream_cmd',
-            default='',
-            help="Command to be executed before synthesis: bitstream",
-            type='')
-        self.add_option(
-            'syn_post_bitstream_cmd',
-            default='',
-            help="Command to be executed after synthesis: bitstream",
-            type='')
-        self.add_option(
-            'syn_post_cmd',
-            default='',
-            help="Command to be executed after synthesis: local",
-            type='')
-        self.add_delimiter()
-        self.add_option(
-            'top_module',
-            default=None,
-            help="Top level entity for synthesis and simulation",
-            type='')
-        self.add_delimiter()
-        self.add_option(
-            'quartus_preflow',
-            default=None,
-            help="Quartus pre-flow script file",
-            type='')
-        self.add_option(
-            'quartus_postmodule',
-            default=None,
-            help="Quartus post-module script file",
-            type='')
-        self.add_option(
-            'quartus_postflow',
-            default=None,
-            help="Quartus post-flow script file",
-            type='')
-        self.add_delimiter()
-        self.add_option(
-            'include_dirs',
-            default=None,
-            help="Include dirs for Verilog sources",
-            type=[])
         self.add_type('include_dirs', type="")
+        self.add_type('incl_makefiles', type='')
+        self.add_type('files', type=[])
+        self.add_allowed_key('modules', key="svn")
+        self.add_allowed_key('modules', key="git")
+        self.add_allowed_key('modules', key="local")
+        fetch_options = [
+            {'name': 'fetchto',
+            'default': None,
+            'help': "Destination for fetched modules",
+            'type': ''},
+            {'name': 'fetch_pre_cmd',
+            'default': '',
+            'help': "Command to be executed before fetch",
+            'type': ''},
+            {'name': 'fetch_post_cmd',
+            'default': '',
+            'help': "Command to be executed after fetch",
+            'type': ''}]
+        self.add_option_list(fetch_options)
         self.add_delimiter()
-        self.add_option(
-            'sim_top',
-            default=None,
-            help="Top level module for simulation",
-            type='')
-        self.add_option(
-            'sim_tool',
-            default=None,
-            help="Simulation tool to be used (e.g. isim, vsim, iverilog)",
-            type='')
-        self.add_option(
-            'sim_pre_cmd',
-            default=None,
-            help="Command to be executed before simulation",
-            type='')
-        self.add_option(
-            'sim_post_cmd',
-            default=None,
-            help="Command to be executed after simulation",
-            type='')
-        self.add_option(
-            'vsim_opt',
-            default="",
-            help="Additional options for vsim",
-            type='')
-        self.add_option(
-            'vcom_opt',
-            default="",
-            help="Additional options for vcom",
-            type='')
-        self.add_option(
-            'vlog_opt',
-            default="",
-            help="Additional options for vlog",
-            type='')
-        self.add_option(
-            'vmap_opt',
-            default="",
-            help="Additional options for vmap",
-            type='')
+        syn_options = [
+            {'name': 'syn_tool',
+            'default': None,
+            'help': "Tool to be used in the synthesis",
+            'type': ''},
+            {'name': 'syn_device',
+            'default': None,
+            'help': "Target FPGA device",
+            'type': ''},
+            {'name': 'syn_family',
+            'default': None,
+            'help': "Target FPGA family",
+            'type': ''},
+            {'name': 'syn_grade',
+            'default': None,
+            'help': "Speed grade of target FPGA",
+            'type': ''},
+            {'name': 'syn_package',
+            'default': None,
+            'help': "Package variant of target FPGA",
+            'type': ''},
+            {'name': 'syn_top',
+            'default': None,
+            'help': "Top level module for synthesis",
+            'type': ''},
+            {'name': 'syn_project',
+            'default': None,
+            'help': "Project file (.xise, .ise, .qpf)",
+            'type': ''},
+            {'name': 'syn_pre_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: local",
+            'type': ''},
+            {'name': 'syn_pre_synthesize_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: synthesize",
+            'type': ''},
+            {'name': 'syn_post_synthesize_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: synthesize",
+            'type': ''},
+            {'name': 'syn_pre_translate_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: translate",
+            'type': ''},
+            {'name': 'syn_post_translate_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: translate",
+            'type': ''},
+            {'name': 'syn_pre_map_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: map",
+            'type': ''},
+            {'name': 'syn_post_map_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: map",
+            'type': ''},
+            {'name': 'syn_pre_par_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: par",
+            'type': ''},
+            {'name': 'syn_post_par_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: par",
+            'type': ''},
+            {'name': 'syn_pre_bitstream_cmd',
+            'default': '',
+            'help': "Command to be executed before synthesis: bitstream",
+            'type': ''},
+            {'name': 'syn_post_bitstream_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: bitstream",
+            'type': ''},
+            {'name': 'syn_post_cmd',
+            'default': '',
+            'help': "Command to be executed after synthesis: local",
+            'type': ''}]
+        self.add_option_list(syn_options)
+        self.add_delimiter()
+        quartus_options = [
+            {'name': 'quartus_preflow',
+            'default': None,
+            'help': "Quartus pre-flow script file",
+            'type': ''},
+            {'name': 'quartus_postmodule',
+            'default': None,
+            'help': "Quartus post-module script file",
+            'type': ''},
+            {'name': 'quartus_postflow',
+            'default': None,
+            'help': "Quartus post-flow script file",
+            'type': ''}]
+        self.add_option_list(quartus_options)
+        self.add_delimiter()
+        sim_options = [
+            {'name': 'sim_top',
+            'default': None,
+            'help': "Top level module for simulation",
+            'type': ''},
+            {'name': 'sim_tool',
+            'default': None,
+            'help': "Simulation tool to be used (e.g. isim, vsim, iverilog)",
+            'type': ''},
+            {'name': 'sim_pre_cmd',
+            'default': None,
+            'help': "Command to be executed before simulation",
+            'type': ''},
+            {'name': 'sim_post_cmd',
+            'default': None,
+            'help': "Command to be executed after simulation",
+            'type': ''}]
+        self.add_option_list(sim_options)
+        self.add_delimiter()
+        modelsim_options = [
+            {'name': 'vsim_opt',
+            'default': "",
+            'help': "Additional options for vsim",
+            'type': ''},
+            {'name': 'vcom_opt',
+            'default': "",
+            'help': "Additional options for vcom",
+            'type': ''},
+            {'name': 'vlog_opt',
+            'default': "",
+            'help': "Additional options for vlog",
+            'type': ''},
+            {'name': 'vmap_opt',
+            'default': "",
+            'help': "Additional options for vmap",
+            'type': ''}]
+        self.add_option_list(modelsim_options)
         self.add_delimiter()
         self.add_option(
             'iverilog_opt',
             default="",
             help="Additional options for IVerilog",
             type='')
+        self.add_delimiter()
         self.add_option(
             'ghdl_opt',
             default="",
             help="Additional options for GHDL",
             type='')
         self.add_delimiter()
-        self.add_option(
-            'modules',
-            default={},
-            help="List of local modules",
-            type={})
-        self.add_allowed_key('modules', key="svn")
-        self.add_allowed_key('modules', key="git")
-        self.add_allowed_key('modules', key="local")
-        self.add_option(
-            'action',
-            default='',
-            help="What is the action that should be taken if "
-                 "HDLMake is run in auto mode (simulation/synthesis)",
-            type='')
-        # self.add_delimiter()
-        self.add_option('library', default="work",
-                        help="Destination library for module's VHDL files",
-                        type="")
-        self.add_option(
-            'files',
-            default=[],
-            help="List of files from the current module",
-            type='')
-        self.add_type('files', type=[])
-        self.add_option(
-            'incl_makefiles',
-            default=[],
-            help="List of .mk files appended to toplevel makefile",
-            type=[])
-        self.add_type('incl_makefiles', type='')
+
+    def add_option_list(self, option_list):
+        """Add to the parser a list with the options and their keys"""
+        for option in option_list:
+            self.add_option(option["name"],
+                            default=option["default"],
+                            help=option["help"],
+                            type=option["type"])
 
     def add_manifest(self, path):
         """Add to configuration the Manifest at directory (path) if exists"""
