d64eb61f3ca1839c1590a42c1344d9cce2dd7560
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2016-08-12 01:56:40 +0200

    We do not need to export the base classes in the Tools package

--- hdlmake/tools/__init__.py
+++ hdlmake/tools/__init__.py
@@ -1,18 +1,11 @@
 """Package that provides all the tool specific stuff"""
 
-from .makefile import ToolMakefile
-
 from .iverilog import ToolIVerilog
 from .isim import ToolISim
 from .modelsim import ToolModelsim
 from .active_hdl import ToolActiveHDL
 from .riviera import ToolRiviera
 from .ghdl import ToolGHDL
-from .sim_makefile_support import VsimMakefileWriter
-
-from .make_syn import ToolSyn
-from .make_sim import ToolSim
-from .xilinx import ToolXilinx
 from .ise import ToolISE
 from .planahead import ToolPlanAhead
 from .vivado import ToolVivado
