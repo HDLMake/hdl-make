eaf8c8ced08a41b2bc636c256ffe320b80a0d827
Javier Garcia <jgarcia@gl-research.com>
2017-03-06 00:23:56 +0100

    Delete extension from project name if provided at the Manifest.py -- fix indentation

--- hdlmake/tools/make_syn.py
+++ hdlmake/tools/make_syn.py
@@ -100,7 +100,7 @@ endif
         self.writeln(top_parameter.substitute(
             tcl_interpreter=tcl_interpreter,
             project_name=os.path.splitext(
-            self.manifest_dict["syn_project"])[0],
+                self.manifest_dict["syn_project"])[0],
             project_ext=self._tool_info["project_ext"],
             tool_path=self.manifest_dict["syn_path"],
             top_module=self.manifest_dict["syn_top"]))
