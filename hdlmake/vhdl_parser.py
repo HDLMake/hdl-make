d0ba0268fc1d9303380daa2b386c046d0634b933
rjen <rjensch@baumer.com>
2017-09-21 14:41:49 +0200

    Modified: - instance regex supports entity instantiation

--- hdlmake/vhdl_parser.py
+++ hdlmake/vhdl_parser.py
@@ -243,8 +243,7 @@ class VHDLParser(DepParser):
         # instantions
         libraries = set([dep_file.library])
         instance_pattern = re.compile(
-            r"^\s*(\w+)\s*\:\s*(\w+)\s*(?:port\s+map.*?;"
-            r"|generic\s+map.*?;|\s*;)",
+            r"^\s*(\w+)\s*:\s*(?:entity\s+\w+\.)?(\w+)\s*(?:port\s+map.*?|generic\s+map.*?)",
             re.DOTALL | re.MULTILINE | re.IGNORECASE)
 
         def do_instance(text):
